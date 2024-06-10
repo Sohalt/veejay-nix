404050391e2e3dd01f3ab1c825bdbddc115f31f5
sohalt <sohalt@sohalt.net>
2024-06-10 16:49:32 -0400

    package as nix apps

--- flake.nix
+++ flake.nix
@@ -1,18 +1,37 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = {
+    self,
     nixpkgs,
     flake-utils,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages = rec {
-        default = veejay-server;
-        veejay-core = pkgs.callPackage ./veejay-core.nix {};
-        veejay-server = pkgs.callPackage ./veejay-server.nix {inherit veejay-core;};
-        veejay-client = pkgs.callPackage ./veejay-client.nix {inherit veejay-core;};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [self.overlays.default];
       };
-    });
+    in
+      {
+        packages = {inherit (pkgs) veejay-core veejay-server veejay-client;};
+        apps = rec {
+          default = veejay-server;
+          veejay-server = {
+            type = "app";
+            program = "${pkgs.veejay-server}/bin/veejay";
+          };
+          veejay-client = {
+            type = "app";
+            program = "${pkgs.veejay-client}/bin/reloaded";
+          };
+        };
+      })
+      // {
+        overlays.default = final: prev: rec {
+          default = veejay-server;
+          veejay-core = final.callPackage ./veejay-core.nix {};
+          veejay-server = final.callPackage ./veejay-server.nix {};
+          veejay-client = final.callPackage ./veejay-client.nix {};
+        };
+      };
 }
